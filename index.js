4fb0b7dc143a6f8a68ac102bdab715866ff982e8
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-08-09 17:54:59 +0200

    Initial setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 8080
+
+// Templating engine
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+// Static files
+app.use(express.static(__dirname + "/public"))
+
+// Routes
+app.use("/", require("./routes/home"))
+
+// Server
+app.listen(port, () => {
+    console.log("Listening on port " + port)
+})
