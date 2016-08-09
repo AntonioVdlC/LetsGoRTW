89b9ea47a35433c9285f4314e98f562b0288fc5a
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-08-09 20:40:56 +0200

    Retrieve the list of countries from the API

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,22 @@
+const fetch = require("node-fetch")
+
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.render("home")
+router.get("/", (req, res, next) => {
+    const urls = [
+        req.protocol + "://" + req.get("host") + "/api/countries"
+    ]
+
+    Promise.all(urls.map(url => 
+        fetch(url).then(data => data.json())
+    ))
+    .then(json => {
+        res.render("home", {
+            countries: json[0]
+        })
+    })
+    .catch(err => next(err))
 })
 
 module.exports = router
